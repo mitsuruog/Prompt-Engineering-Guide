1815413bd394a76c4a5820505ec6b20b535219b1
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2023-03-24 13:50:11 +0900

    WIP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withNextra({
+  images: { unoptimized: true },
+  basePath: isProd ? '/Prompt-Engineering-Guide' : '',
+  assetPrefix: isProd ? '/Prompt-Engineering-Guide' : '',
+  trailingSlash: true
+});
